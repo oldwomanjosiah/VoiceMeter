cc8f81ffd76a13521c0253dc7345b52b93ae1299
Josiah Hilden <jhilden13@gmail.com>
2023-11-20 18:58:37 -0600

    Clean up old code

--- src/connection.rs
+++ src/connection.rs
@@ -166,6 +166,8 @@ pub mod buffer {
 
     #[cfg(test)]
     mod tests {
+        use std::hash::BuildHasher;
+
         use cpal::SampleRate;
 
         use super::*;
@@ -175,14 +177,25 @@ pub mod buffer {
             let buffer = SampleBuffer::<i32>::new(SampleRate(1));
         }
 
+        #[test]
         fn buffer_samples() {
             let mut buffer = SampleBuffer::<i32>::new(SampleRate(1));
-            buffer.ingest(std::array::from_fn::<_, 32, _>(|idx| idx as _));
+            let data = std::array::from_fn::<_, 32, _>(|idx| idx as _);
+            buffer.ingest(&data);
+
+            assert_eq!(buffer.backbuffer().copied().collect::<Vec<_>>(), []);
+
+            let new_samples = buffer.take(32).copied().collect::<Vec<_>>();
+            assert_eq!(new_samples, &data);
+
+            assert_eq!(buffer.backbuffer().copied().collect::<Vec<_>>(), &data);
+
+            buffer.trim_backbuffer(16);
+            assert_eq!(buffer.backbuffer().copied().collect::<Vec<_>>(), &data[16..32]);
         }
     }
 }
 
-type InitialTimestamp = Arc<std::sync::OnceLock<cpal::InputStreamTimestamp>>;
 type Tx<Sample> =
     thingbuf::mpsc::blocking::Sender<channels::ChannelInfo<Sample>, channels::ChannelInfoRecycler>;
 type Rx<Sample> = thingbuf::mpsc::blocking::Receiver<
@@ -442,166 +455,6 @@ impl<S, I> ChannelConnection<S, I> {
     pub fn name(&self) -> &str { self.name.as_ref() }
 }
 
-pub struct MeteredConnection<S, I> {
-    #[allow(unused)]
-    stream: S,
-    name: Cow<'static, str>,
-    config: cpal::StreamConfig,
-
-    recv: Receiver<Result<Vec<I>, StreamError>>,
-    reuse: SyncSender<Vec<I>>,
-
-    buffer: VecDeque<I>,
-    errors: Vec<StreamError>,
-}
-
-impl<S, I> MeteredConnection<S, I> {
-    pub fn process(&mut self)
-    where
-        I: Copy,
-    {
-        let mut samples = 0;
-        while let Ok(next) = self.recv.try_recv() {
-            match next {
-                Ok(mut buffer) => {
-                    self.buffer.extend(buffer.iter().copied());
-                    samples += buffer.len();
-                    buffer.clear();
-                    self.reuse.try_send(buffer).ok();
-                }
-
-                Err(e) => self.errors.push(e),
-            }
-        }
-
-        eprintln!("Got {samples} samples");
-    }
-
-    pub fn channels_for_frame(&mut self, frame_time: Duration) -> MeteredRef<'_, S, I> {
-        let mut speeding = false;
-        let mut samples =
-            (frame_time.as_secs_f64() * (self.config.sample_rate.0 as f64)).ceil() as usize;
-
-        if self.buffered_time() > Duration::from_millis(500) {
-            speeding = true;
-            samples = samples * 3 / 2;
-        }
-
-        let channels = self.config.channels as usize;
-        samples *= channels;
-
-        eprintln!(
-            "Taking {samples} samples for frametime {}ms (speeding: {speeding:?})",
-            frame_time.as_millis()
-        );
-
-        MeteredRef {
-            conn: self,
-            channels,
-            samples,
-        }
-    }
-
-    pub fn buffered_time(&self) -> Duration {
-        let samples = self.buffer.len() as _;
-        let channels = self.config.channels as _;
-        let rate = self.config.sample_rate.0 as _;
-
-        Duration::from_secs(1) * samples / channels / rate
-    }
-
-    pub fn name(&self) -> &str {
-        &self.name
-    }
-}
-
-pub struct MeteredRef<'c, S, I> {
-    conn: &'c mut MeteredConnection<S, I>,
-    channels: usize,
-    samples: usize,
-}
-
-impl<S, I> Drop for MeteredRef<'_, S, I> {
-    fn drop(&mut self) {
-        self.conn
-            .buffer
-            .drain(0..self.samples.min(self.conn.buffer.len()));
-    }
-}
-
-impl<S, I> MeteredRef<'_, S, I> {
-    pub fn channel_iter(&self, channel: usize) -> impl Iterator<Item = &'_ I> {
-        assert!(
-            channel < self.channels,
-            "index {channel} out of bounds for {} channels",
-            self.channels
-        );
-
-        self.conn
-            .buffer
-            .iter()
-            .take(self.samples) // Total number for all channels
-            .skip(channel) // Offset into iter by channel number
-            .step_by(self.channels) // Skip along by the total number of channels, so we only get the data for this channel
-    }
-
-    pub fn channels(&self) -> usize {
-        self.channels
-    }
-}
-
-impl<S: cpal::traits::StreamTrait, I: AllConvertable + Send> MeteredConnection<S, I> {
-    pub fn new<D>(device: D, repaint: impl FnMut() + Send + 'static) -> Result<Self>
-    where
-        D: cpal::traits::DeviceTrait<Stream = S>,
-    {
-        let name = device
-            .name()
-            .map(Cow::Owned)
-            .unwrap_or(Cow::Borrowed("<unknown>"));
-
-        let supported_config = device
-            .default_input_config()
-            .wrap_err("getting default config")?;
-        let config = supported_config.config();
-        let config = cpal::StreamConfig {
-            buffer_size: cpal::BufferSize::Fixed(128),
-            ..config
-        };
-
-        let (tx, recv) = std::sync::mpsc::channel();
-        let (reuse, get_reused) = std::sync::mpsc::sync_channel(32);
-
-        let span = Arc::new(
-            tracing::info_span!("Input Stream Worker", %name, ty = ?supported_config.sample_format()),
-        );
-
-        let stream = connect_sending(
-            &device,
-            &supported_config,
-            &config,
-            Some(span),
-            tx,
-            get_reused,
-            repaint,
-        )?;
-
-        stream.play().wrap_err("starting stream")?;
-
-        let conn = MeteredConnection {
-            stream,
-            name,
-            config,
-            recv,
-            reuse,
-            buffer: VecDeque::new(),
-            errors: Vec::new(),
-        };
-
-        Ok(conn)
-    }
-}
-
 macro_rules! def_all_convert {
     ($($ty:ty),+ $(,)?) => {
         pub trait AllConvertable:
@@ -612,158 +465,3 @@ macro_rules! def_all_convert {
 }
 
 def_all_convert![f32, f64, i8, i16, i32, i64, u8, u16, u32, u64];
-
-/// Connect to device with a given configuration, converting whatever sample format it supports into a common sample format of your choice.
-fn connect_sending<
-    S: AllConvertable + Send,
-    Sp: AsRef<tracing::Span> + Clone + Send + 'static,
-    D: cpal::traits::DeviceTrait,
->(
-    device: &D,
-    supported_config: &cpal::SupportedStreamConfig,
-    config: &cpal::StreamConfig,
-    span: Option<Sp>,
-    sender: Sender<Result<Vec<S>, cpal::StreamError>>,
-    reuse: Receiver<Vec<S>>,
-    repaint: impl FnMut() + Send + 'static,
-) -> Result<D::Stream, cpal::BuildStreamError> {
-    let err_fn = send_err(span.clone(), sender.clone());
-
-    match supported_config.sample_format() {
-        cpal::SampleFormat::I8 => device.build_input_stream::<i8, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::I16 => device.build_input_stream::<i16, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::I32 => device.build_input_stream::<i32, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::I64 => device.build_input_stream::<i64, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::U8 => device.build_input_stream::<u8, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::U16 => device.build_input_stream::<u16, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::U32 => device.build_input_stream::<u32, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::U64 => device.build_input_stream::<u64, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::F32 => device.build_input_stream::<f32, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        cpal::SampleFormat::F64 => device.build_input_stream::<f64, _, _>(
-            config,
-            call_with_each(sample_function(span, sender, reuse), repaint),
-            err_fn,
-            None,
-        ),
-
-        it => {
-            tracing::warn!("Stream format {it:?} is not known");
-            Err(cpal::BuildStreamError::StreamConfigNotSupported)
-        }
-    }
-}
-
-fn call_with_each<T>(
-    mut base: impl FnMut(&[T], &InputCallbackInfo),
-    mut on_each: impl FnMut(),
-) -> impl FnMut(&[T], &InputCallbackInfo) {
-    move |data, info| {
-        base(data, info);
-        on_each();
-    }
-}
-
-fn send_err<S, E: std::error::Error, Sp: AsRef<tracing::Span>>(
-    span: Option<Sp>,
-    sender: Sender<Result<S, E>>,
-) -> impl FnMut(E) {
-    move |err| {
-        let _guard = span.as_ref().map(AsRef::as_ref).map(tracing::Span::enter);
-
-        tracing::error!(%err, "Got Error from stream");
-
-        if let Err(err) = sender.send(Err(err)) {
-            tracing::warn!(%err, "Could not send error");
-        }
-    }
-}
-
-fn sample_function<I, T, E, Sp: AsRef<tracing::Span>>(
-    span: Option<Sp>,
-    sender: Sender<Result<Vec<T>, E>>,
-    reuse: Receiver<Vec<T>>,
-) -> impl FnMut(&[I], &InputCallbackInfo)
-where
-    T: cpal::FromSample<I>,
-    I: Copy,
-{
-    move |samples, _info| {
-        let _guard = span.as_ref().map(AsRef::as_ref).map(tracing::Span::enter);
-
-        tracing::trace!(count = samples.len(), "Recieved Samples");
-
-        let mut out = match reuse.try_recv().ok() {
-            Some(mut reuse) => {
-                tracing::trace!("Reusing buffer");
-                reuse.clear();
-                reuse.reserve(samples.len());
-                reuse
-            }
-
-            None => {
-                tracing::debug!("Building new buffer");
-                Vec::with_capacity(samples.len())
-            }
-        };
-
-        out.extend(samples.iter().copied().map(cpal::FromSample::from_sample_));
-
-        if let Err(e) = sender.send(Ok(out)) {
-            tracing::warn!(%e, "Could not send data");
-        }
-    }
-}
