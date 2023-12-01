f62e5c6234db6924d3ca343ae2f91dbd88af6884
Josiah Hilden <jhilden13@gmail.com>
2023-12-01 17:45:38 -0600

    FFT display (probably incorrect)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::{sync::Arc, time::Duration};
 
+use cpal::Device;
 use eyre::{Context, Result};
+use rustfft::{num_complex::Complex32, Fft, FftPlanner};
 use voicemeter::{
     connection::{
-        buffer::SampleBuffer,
+        buffer::{self, SampleBuffer},
         managed::{LoadError, ManagedConnection, Manager},
     },
-    ui::{DynamicLce, Lce, LoadableExt},
+    ui::{DynamicLce, HorizontalBarWidget, Lce, LoadableExt},
 };
+
+struct ChannelAnalysisSet {
+    devices: Vec<ChannelAnalysis>,
+    detail_view: Option<(usize, usize)>,
+
+    forier: Arc<dyn Fft<f32>>,
+    forier_buffer: Vec<Complex32>,
+    forier_scratch: Vec<Complex32>,
+}
 
 struct ChannelAnalysis {
     pub connection: ManagedConnection<i32>,
@@ -24,6 +35,250 @@ struct BarInfo<'o> {
     pub jagged: f32,
     pub smooth: f32,
     pub decaying: f32,
+}
+
+impl ChannelAnalysisSet {
+    pub fn new(from: Vec<ManagedConnection<i32>>, planner: &mut FftPlanner<f32>) -> Self {
+        let buffer_len = 2 << 11;
+        let forier = planner.plan_fft_forward(buffer_len);
+
+        let forier_buffer = vec![Complex32::new(0.0, 0.0); buffer_len];
+        let forier_scratch = vec![Complex32::new(0.0, 0.0); forier.get_inplace_scratch_len()];
+
+        Self {
+            devices: from.into_iter().map(ChannelAnalysis::new).collect(),
+
+            detail_view: None,
+
+            forier,
+            forier_buffer,
+            forier_scratch,
+        }
+    }
+
+    pub fn rectify_selected(&mut self) {
+        self.detail_view = self.detail_view.and_then(|(device, channel)| {
+            let Some(dev) = self.devices.get(device) else {
+                return None;
+            };
+
+            (channel < dev.connection.channels()).then_some((device, channel))
+        });
+    }
+
+    pub fn detail_channel_mut(&mut self) -> Option<&mut SampleBuffer<i32>> {
+        self.rectify_selected();
+
+        let (device, channel) = self.detail_view?;
+
+        Some(&mut self.devices[device].connection.buffers()[channel])
+    }
+
+    pub fn process_fft(
+        &mut self,
+    ) -> Option<impl ExactSizeIterator<Item = (usize, Complex32)> + '_> {
+        self.rectify_selected();
+        let (device, channel) = self.detail_view?;
+        let data = &mut self.devices[device].connection.buffers()[channel];
+
+        let buffer_len = self.forier_buffer.len();
+        let backbuffer_len = data.backbuffer_len();
+        if backbuffer_len < buffer_len {
+            return None;
+        }
+
+        // let skip_amount = data.backbuffer_len().saturating_sub(buffer_len);
+
+        // let backbuffer = data.backbuffer().skip(skip_amount).take(buffer_len);
+
+        // let initial_idx = (backbuffer.len() - buffer_len) / 2;
+
+        // if backbuffer.len() < buffer_len {
+        //     tracing::warn!(len = %backbuffer.len(), expected = %buffer_len, "Backbuffer does not contain enough data!");
+        // }
+
+        // let iter = std::iter::repeat(0)
+        //     .take(initial_idx)
+        //     .chain(backbuffer.copied())
+        //     .chain(std::iter::repeat(0));
+
+        let iter = data
+            .backbuffer()
+            .skip(backbuffer_len.saturating_sub(buffer_len))
+            .copied();
+
+        for (idx, (val, buf)) in iter.zip(&mut self.forier_buffer).enumerate() {
+            fn convert(val: i32) -> f32 {
+                (val as f32) / (i32::MAX as f32)
+            }
+
+            fn real(val: f32) -> Complex32 {
+                Complex32::new(val, 0.0)
+            }
+
+            fn filter(idx: usize, len: usize, val: f32) -> f32 {
+                let half_len = len as f32 / 2.0;
+                let percent = 1.0 - ((idx as f32 - half_len).abs() / half_len);
+                assert!(0.0 <= percent && percent <= 1.0);
+                // val * percent
+                val
+            }
+
+            *buf = real(filter(idx, buffer_len, convert(val)));
+        }
+
+        self.forier
+            .process_with_scratch(&mut self.forier_buffer, &mut self.forier_scratch);
+
+        let scale = (buffer_len as f32).sqrt();
+
+        struct Alternating<I> {
+            next_back: bool,
+            inner: I,
+        }
+
+        impl<I> Iterator for Alternating<I>
+        where
+            I: DoubleEndedIterator<Item = Complex32>,
+        {
+            type Item = I::Item;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                let a = self.inner.next()?;
+                let b = self.inner.next_back()?;
+                Some((a + b) / 2.0)
+            }
+        }
+
+        impl<I> ExactSizeIterator for Alternating<I>
+        where
+            Self: Iterator,
+            I: ExactSizeIterator,
+        {
+            fn len(&self) -> usize {
+                self.inner.len() / 2
+            }
+        }
+
+        impl<I> Alternating<I> {
+            fn new(inner: I) -> Self {
+                Self {
+                    inner,
+                    next_back: false,
+                }
+            }
+        }
+
+        Some(
+            Alternating::new(
+                self.forier_buffer
+                    .iter()
+                    .map(move |it| (it).scale(1.0 / scale)),
+            )
+            .enumerate(),
+        )
+    }
+
+    pub fn showing_detail(&self) -> bool {
+        self.detail_view.is_some()
+    }
+
+    pub fn list_panel(&mut self, ui: &mut egui::Ui) {
+        let frame_time = Duration::from_secs_f32(ui.ctx().input(|it| it.unstable_dt));
+
+        egui::ScrollArea::vertical().show(ui, |ui| {
+            for (dev_idx, device) in self.devices.iter_mut().enumerate() {
+                device.process(frame_time);
+
+                ui.separator();
+                ui.heading(device.connection.name());
+
+                device.with_bar_info(frame_time, |channel_idx, channel_info| {
+                    let sense = ui
+                        .push_id(dev_idx * 31 + channel_idx, |ui| {
+                            ui.add(
+                                HorizontalBarWidget::new(
+                                    channel_info.jagged,
+                                    channel_info.smooth,
+                                    channel_info.decaying,
+                                )
+                                .with_label(channel_idx.to_string()),
+                            );
+                        })
+                        .response
+                        .interact(egui::Sense::click());
+
+                    if sense.clicked() {
+                        self.detail_view = Some((dev_idx, channel_idx));
+                    }
+
+                    if sense.hovered()
+                        || self
+                            .detail_view
+                            .is_some_and(|it| it.0 == dev_idx && it.1 == channel_idx)
+                    {
+                        ui.label("Selected TODO fix this");
+                    }
+                });
+            }
+        });
+    }
+
+    pub fn detail_view(&mut self, ui: &mut egui::Ui) {
+        egui_plot::Plot::new("fft_plot").show(ui, |plot| {
+            plot.set_plot_bounds(egui_plot::PlotBounds::from_min_max(
+                [0.0, 0.0],
+                [f64::INFINITY, 1.0],
+            ));
+            plot.set_auto_bounds([true, false].into());
+
+            let Some(points) = self.process_fft() else {
+                // ui.label("No FFT");
+                return;
+            };
+
+            let points_len = points.len();
+            let mut mag = Vec::with_capacity(points.len());
+
+            for (idx, point) in points {
+                use egui_plot::PlotPoint;
+
+                // let x = (points_len as f64).log10() - ((points_len - idx) as f64).log10();
+                let x = ((idx + 1) as f64).log10();
+                // let x = idx as f32;
+
+                mag.push(PlotPoint::new(x, point.norm()));
+            }
+
+            use egui::Color32;
+            use egui_plot::Line;
+            use egui_plot::PlotPoints;
+
+            plot.line(Line::new(PlotPoints::Owned(mag)).color(Color32::RED));
+        });
+    }
+}
+
+impl egui::Widget for &mut ChannelAnalysisSet {
+    fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+        if self.showing_detail() {
+            voicemeter::ui::HorizontalSplit::new("detail_view")
+                .show_with(
+                    ui,
+                    self,
+                    |ui, this| {
+                        this.list_panel(ui);
+                    },
+                    |ui, this| {
+                        this.detail_view(ui);
+                    },
+                )
+                .response
+        } else {
+            ui.allocate_ui(ui.available_size_before_wrap(), |ui| self.list_panel(ui))
+                .response
+        }
+    }
 }
 
 impl ChannelAnalysis {
@@ -85,19 +340,16 @@ impl ChannelAnalysis {
 
 struct App {
     manager: Manager<i32>,
-    connections: DynamicLce<Vec<ChannelAnalysis>, LoadError>,
+    connections: DynamicLce<ChannelAnalysisSet, LoadError>,
+    fft_planner: FftPlanner<f32>,
 }
 
 impl App {
     fn reload_connections(&mut self) {
         let loader = self.manager.load();
         let mapped_loader = loader.map_res(|res| {
-            res.map(|connections| {
-                connections
-                    .into_iter()
-                    .map(ChannelAnalysis::new)
-                    .collect::<Vec<_>>()
-            })
+            let mut planner = FftPlanner::new();
+            res.map(|connections| ChannelAnalysisSet::new(connections, &mut planner))
         });
 
         self.connections = Lce::Loading(mapped_loader).to_dyn_lce();
@@ -109,6 +361,7 @@ impl App {
         App {
             manager,
             connections: Default::default(),
+            fft_planner: FftPlanner::new(),
         }
     }
 }
@@ -141,23 +394,24 @@ impl eframe::App for App {
                         });
                     },
                     |ui, c| {
-                        for connection in c {
-                            connection.process(frametime);
+                        ui.add(c);
+                        // for connection in c {
+                        //     connection.process(frametime);
 
-                            ui.separator();
-                            ui.label(connection.connection.name());
+                        //     ui.separator();
+                        //     ui.label(connection.connection.name());
 
-                            connection.with_bar_info(frametime, |idx, info| {
-                                ui.add(
-                                    voicemeter::ui::HorizontalBarWidget::new(
-                                        info.jagged,
-                                        info.smooth,
-                                        info.decaying,
-                                    )
-                                    .with_label(format!("{idx}")),
-                                );
-                            });
-                        }
+                        //     connection.with_bar_info(frametime, |idx, info| {
+                        //         ui.add(
+                        //             voicemeter::ui::HorizontalBarWidget::new(
+                        //                 info.jagged,
+                        //                 info.smooth,
+                        //                 info.decaying,
+                        //             )
+                        //             .with_label(format!("{idx}")),
+                        //         );
+                        //     });
+                        // }
                     },
                     |ui, e| {
                         ui.heading("Error");
--- src/ui.rs
+++ src/ui.rs
@@ -2,4 +2,50 @@ mod horizontal_bar;
 mod lce;
 
 pub use horizontal_bar::*;
+pub use horizontal_split::*;
 pub use lce::*;
+
+mod horizontal_split {
+    use std::hash::Hash;
+
+    use egui::{Id, InnerResponse, Ui};
+
+    pub struct HorizontalSplit {
+        id: Id,
+    }
+
+    impl HorizontalSplit {
+        pub fn new<IdSource: Hash>(id_source: IdSource) -> Self {
+            let id = Id::new(id_source);
+
+            Self { id }
+        }
+
+        pub fn show<Rl, Rr>(
+            self,
+            ui: &mut Ui,
+            left_content: impl FnOnce(&mut Ui) -> Rl,
+            right_content: impl FnOnce(&mut Ui) -> Rr,
+        ) -> InnerResponse<(Rl, Rr)> {
+            self.show_with(ui, (), |ui, _| left_content(ui), |ui, _| right_content(ui))
+        }
+
+        pub fn show_with<C, Rl, Rr>(
+            self,
+            ui: &mut Ui,
+            mut ctx: C,
+            left_content: impl FnOnce(&mut Ui, &mut C) -> Rl,
+            right_content: impl FnOnce(&mut Ui, &mut C) -> Rr,
+        ) -> InnerResponse<(Rl, Rr)> {
+            let total_rect = ui.available_rect_before_wrap();
+            let (left_rect, right_rect) = total_rect.split_left_right_at_fraction(0.5);
+            let left_resp = ui.allocate_ui_at_rect(left_rect, |ui| left_content(ui, &mut ctx));
+            let right_resp = ui.allocate_ui_at_rect(right_rect, |ui| right_content(ui, &mut ctx));
+
+            InnerResponse {
+                inner: (left_resp.inner, right_resp.inner),
+                response: left_resp.response.union(right_resp.response),
+            }
+        }
+    }
+}
